ffc137f5c7a6e41c84f6e751fff299a96d523100
nicolasferry <nicolas.ferry@sintef.no>
2018-04-13 09:52:55 +0200

    fix crappy merging

--- nodes/savvy/savvy.js
+++ nodes/savvy/savvy.js
@@ -37,22 +37,22 @@ module.exports = function (RED) {
 
             sendGet(msg, method, node, function (res) {
 				
-		console.log("Callback sendGet");
+				console.log("Callback sendGet");
                 var msg = {};
-		switch(node.returntype) {
-			case "utf-8":
-				msg.payload = res;
-				break;
-			case "json":
-				msg.payload = JSON.parse(res);
-				break;
-		}
-				
-		// Send the CNC manufacturer as msg parameter
-		msg.cncmanufacturer = node.cncmanufacturer;
-                node.send(msg);
+				switch(node.returntype) {
+					case "utf-8":
+						msg.payload = res;
+						break;
+					case "json":
+						msg.payload = JSON.parse(res);
+						break;
+				}
+						
+				// Send the CNC manufacturer as msg parameter
+				msg.cncmanufacturer = node.cncmanufacturer;
+				node.send(msg);
             });
-        });
+		});
 
         this.on("close", function () {
             // Called when the node is shutdown - eg on redeploy.
@@ -189,125 +189,6 @@ module.exports = function (RED) {
 				});
 				break;
 		}
-		
-        req.on('error', function (err) {
-            console.log("Connection not open " + err);
-        });
-
-        req.end();
-    }
-
-    function generateAuthorization(loc, epoch, key, secret)
-	{
-        var Request = "GET" + "\n" + "text/plain; charset=UTF-8" + "\n" + epoch + "\n" + loc;
-        var authorization = "M2C" + " " + key + ":" + generateHmac(Request, secret);
-
-        return authorization;
-    }
-
-    function generateHmac(data, secret, algorithm, encoding)
-	{
-        var encoding = encoding || "base64";
-        var algorithm = algorithm || "sha1";
-
-        return crypto.createHmac(algorithm, secret).update(data).digest(encoding);
-    }
-
-    // Register the node by name. This must be called before overriding any of the
-    // Node functions.
-    RED.nodes.registerType("savvy", savvyNode);
-
-}
-
-    function computeEndpoint(node) {
-        var ep = "";
-		
-		if (node.apitype === 'cloud')
-		{
-			switch(node.target)
-			{
-				case "stream":
-					ep += "/v1/stream?track=" + node.machineid;
-					break;
-				case "machines":
-					ep += "/v1/locations/" + node.locationid + "/machines";
-					break;
-				case "indicators":
-					ep += "/v1/locations/" + node.locationid + "/machines/" + node.machineid + "/groups/" + node.groupid + "/indicators";
-					break;
-				case "groups":
-					ep += "/v1/locations/" + node.locationid + "/machines/" + node.machineid + "/groups";
-					break;
-				default:
-					ep += "/v1/locations/";
-			}
-		}
-		else if (node.apitype === 'local')
-		{
-			ep += "/stream?machines=" + node.machineid;
-		}
-		
-        return ep;
-    }
-
-    function sendGet(msg, path, node, callback) {
-        var epoch = new Date().getTime();
-        var auth = generateAuthorization(path, epoch, node.key, node.secret);
-
-		switch(node.apitype) 
-		{
-			case "cloud":
-				var options = {
-					host: node.endpoint,
-					path: path,
-					method: "GET",
-					"headers": {
-						"Content-Type": "text/plain; charset=UTF-8",
-						"X-M2C-Sequence": epoch,
-						"Authorization": auth
-					}
-				};
-				break;
-				
-			// Local restStreaming requires a port and doesn't need extra headers
-			case "local":
-				var options = {
-					host: node.endpoint,
-					port: node.port,
-					path: path,
-					method: "GET",
-					"headers": {
-						"Content-Type": "text/plain; charset=UTF-8"
-					}
-				};
-				break;
-		}
-
-        var req = https.request(options, function (response) {
-			
-            var str = ''
-            response.on('data', function (chunk) {
-			
-				// If we are streaming (cloud or local), we return the chunk itself as the full response is in one chunk
-				if (node.target === "stream" || node.apitype === 'local')
-				{	
-					// The '' + chunk is added to convert the buffer into a string, and the result is always on the pos [1] of the splitted chunk. 
-					callback(('' + chunk).split("\r\n")[1]);			
-				}					
-				else
-				{
-					str += chunk;
-				}					
-            });
-
-            response.on('end', function () {
-                callback(str);
-            });
-
-            response.on('error', function (e) {
-                console.log(e);
-            });
-        });
 		
         req.on('error', function (err) {
             console.log("Connection not open " + err);
